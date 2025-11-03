406322186f1d20b921ee521cc21400aa48871d38
DanielSelhorst <danicandy123@gmail.com>
2025-11-03 19:44:53 -0300

    Delete projeto_cad_usuarios directory

--- projeto_cad_usuarios/colaboradores/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ColaboradoresConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'colaboradores'
--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path('',views.home, name='home')
-    path('', include('colaboradores.urls')),
-]
-   
